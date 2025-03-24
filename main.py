356be3ac2b4e6bfdaf731c29350af59c506adfe5
Darian Cusco <cusco.d@gmail.com>
2025-03-24 18:51:47 -0400

    Added simple endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from firebase_config import db
 
 app = FastAPI()
@@ -9,8 +9,28 @@ def get_recipes():
     recipes = [doc.to_dict() for doc in recipes_ref.stream()]
     return {"recipes": recipes}
 
+@app.get("/recipe/{id}")
+def get_recipe(id: int):
+    recipe_ref = db.collection("recipes").document(str(id))
+    recipe = recipe_ref.get()
+
+    if recipe.exists:
+        return {"recipe": recipe.to_dict()}
+    else:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    
 @app.get("/users")
 def get_users():
     users_ref = db.collection("users")
     users = [doc.to_dict() for doc in users_ref.stream()]
     return {"users": users}
+
+@app.get("/user/{id}")
+def get_recipe(id: int):
+    user_ref = db.collection("users").document(str(id))
+    user = user_ref.get()
+
+    if user.exists:
+        return {"user": user.to_dict()}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
